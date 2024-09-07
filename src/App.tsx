a5f07249ff1aef9f3b0f6cfcb12ce6273c9e67ae
A <movie3342@gmail.com>
2024-09-07 15:19:32 +0900

    feat: hello react fiber

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import * as THREE from "three";
+
+import { useRef, useState } from "react";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+
+function Box(props: ThreeElements["mesh"]) {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+const App = () => {
+  return (
+    <Canvas shadows camera={{ position: [4, 2.5, 8], fov: 35 }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      <OrbitControls />
+      <Environment preset="city" />
+    </Canvas>
+  );
+};
+
+export default App;
